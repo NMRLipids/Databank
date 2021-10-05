043aee36526aded14270c2027b5afbefddd77db6
Anne Kiirikki <akiirikk@dx8-919-002.biocenter.helsinki.fi>
2021-10-05 17:13:14 +0300

    searchDatabank.py works, QualityEvaluation.py does not

--- Scripts/BuildDatabank/searchDATABANK.py
+++ Scripts/BuildDatabank/searchDATABANK.py
@@ -8,56 +8,58 @@ import math
 from matplotlib import cm
 from scipy.stats import norm
 
-
+from databankLibrary import lipids_dict
 
 databank_path = '../../Data/Simulations'
 
 
-lipid_numbers_list = ['POPC', 'POPG', 'POPS', 'POPE', 'CHOL', 'DPPC', 'DHMDMAB', 'DMPC', 'POPI'] # should contain all lipid names
+lipid_numbers_list =  lipids_dict.keys() # should contain all lipid names
 ions_list = ['POT', 'SOD', 'CLA', 'CAL'] # should contain names of all ions
 
+class Data:
+    def __init__(self, molecule, data_path):
+        self.molecule = molecule
+        self.data = {}
+        self.__load_data__(data_path)
+    
+    def __load_data__(self,data_path):
+        with open(data_path) as json_file:
+            self.data = json.load(json_file)
+        json_file.close()
 
+        
 class Simulation:
     def __init__(self, readme, data, indexingPath):
         self.readme = readme
-        self.data = data #dictionary where key is the lipid type and value is order parameter file
+        self.data = data #object Data
         self.indexingPath = indexingPath
     
     def getLipids(self, molecules=lipid_numbers_list):
         lipids = []
-        for key in molecules:
-            try:
-                if self.readme['N'+key] != [0,0]: 
-                    lipids.append(key)
-            except KeyError:
-                continue
+        
+        for key in self.readme['COMPOSITION'].keys():
+            if key in molecules:
+                lipids.append(key)
+
         return lipids
     
     def getIons(self, ions):
         simIons = []
-        
-        for key in ions:
-           try:
-               if self.readme['N'+key] != 0:
-                   simIons.append(key)
-           except KeyError:
-               continue
+        for key in self.readme['COMPOSITION'].keys():
+            if key in ions:
+                simIons.append(key)
                 
         return simIons
 
     #fraction of each lipid with respect to total amount of lipids      
     def molarFraction(self, molecule,molecules=lipid_numbers_list): #only for lipids
         sum_lipids = 0
-        for key in molecules:
-            try:
-                #print(readme[key])
-                sum_lipids += sum(self.readme['N'+key])
-            except KeyError:
-                continue
-        #print("N" + molecule +": " + str(self.readme['N'+molecule]))
+        number = sum(self.readme['COMPOSITION'][molecule]['COUNT']) 
         
-        number = sum(self.readme['N'+molecule]) 
-    
+        for key in self.readme['COMPOSITION'].keys():
+            if key in molecules:
+                sum_lipids += sum(self.readme['COMPOSITION'][key]['COUNT'])
+
         return number / sum_lipids
         
         
@@ -66,9 +68,9 @@ class Simulation:
     def ionConcentration(self, molecule, exp_counter_ions):
         lipids1 = self.getLipids()
         c_water = 55.5
-        N_water = self.readme['NSOL']
+        N_water = self.readme['COMPOSITION']['SOL']['COUNT']
         try:
-            N_molecule = self.readme['N'+molecule] #number of ions
+            N_molecule = self.readme['COMPOSITION'][molecule]['COUNT'] #number of ions
         except KeyError:
             N_molecule = 0
         
@@ -77,7 +79,7 @@ class Simulation:
         if exp_counter_ions and N_molecule != 0:
             for lipid in lipids1:
                 if molecule in exp_counter_ions.keys() and lipid == exp_counter_ions[molecule]:
-                    N_lipid = self.readme['N'+lipid]
+                    N_lipid = self.readme['COMPOSITION'][lipid]['COUNT']
                  #   print(molecule + " " + lipid)
                  #   print(self.readme)
                     lipids2.append(sum(N_lipid))
@@ -93,10 +95,10 @@ class Simulation:
     def totalLipidConcentration(self):
         lipids = self.getLipids()
         c_water = 55.5
-        N_water = self.readme['NSOL']
+        N_water = self.readme['COMPOSITION']['SOL']['COUNT']
         N_lipids = 0
         for lipid in lipids:
-            N_lipids += sum(self.readme['N'+lipid])
+            N_lipids += sum(self.readme['COMPOSITION'][lipid]['COUNT'])
         try:
             if (N_water / N_lipids) > 25 :
                 tot_lipid_c = 'full hydration'
@@ -111,7 +113,7 @@ class Simulation:
 class Experiment:
     def __init__(self, readme, data, dataPath):
         self.readme = readme
-        self.data = data #dictionary where key is the lipid type and value is order parameter file
+        self.data = data #object Data
         self.dataPath = dataPath
         
     def getLipids(self, molecules=lipid_numbers_list):
@@ -153,20 +155,22 @@ for subdir, dirs, files in os.walk(r'../../Data/Simulations/'):
             with open(READMEfilepathSimulation) as yaml_file_sim:
                 readmeSim = yaml.load(yaml_file_sim, Loader=yaml.FullLoader)
                 indexingPath = "/".join(filepath.split("/")[4:8])
-                simOPdata = {} #order parameter files for each type of lipid
+                simOPdata = [] #order parameter files for each type of lipid
                 for filename2 in files:
                     if filename2.endswith('OrderParameters.json'):
-                        key_data1 = filename2.replace('OrderParameters.json', '')
-                        OPfilepath = subdir + '/' + filename2
-                        with open(OPfilepath) as json_file:
-                            simOPdata[key_data1] = json.load(json_file)
-                            json_file.close()
+                        lipid_name = filename2.replace('OrderParameters.json', '')
+                        dataPath = subdir + '/' + filename2
+                        op_data = Data(lipid_name, dataPath)
+                        simOPdata.append(op_data)
                 simulations.append(Simulation(readmeSim, simOPdata, indexingPath))
                 yaml_file_sim.close()
 
 # make empty dictionary for saving paths to matching experimental data entries                
 for simulation in simulations:
     simulation.readme['EXPERIMENT'] = {}
+    for lipid in simulation.getLipids():
+        simulation.readme['EXPERIMENT'][lipid] = {}
+        
     outfileDICT = '../../Data/Simulations/'+ simulation.indexingPath + '/README.yaml'
     with open(outfileDICT, 'w') as f:
         yaml.dump(simulation.readme,f, sort_keys=False)
@@ -181,58 +185,50 @@ for exp_subdir, exp_dirs, exp_files in os.walk(r'../../Data/experiments/'):
             #print(READMEfilepathExperiment)
             with open(READMEfilepathExperiment) as yaml_file_exp:
                 readmeExp = yaml.load(yaml_file_exp, Loader=yaml.FullLoader)
-                expOPdata = {}
                 for filename2 in exp_files:
                     if filename2.endswith('Order_Parameters.json'):
-                        key_data2 = filename2.replace('_Order_Parameters.json','')
-                        #print(key_data2)
+                        lipid_name = filename2.replace('_Order_Parameters.json','')
+                        #print(lipid_name)
                         dataPath = exp_subdir + '/' + filename2 #json!
-                        print(dataPath)
-                        with open(dataPath) as json_file:
-                            expOPdata[key_data2] = json.load(json_file)
-                            json_file.close()
-                experiments.append(Experiment(readmeExp, expOPdata, exp_subdir))
+                        expOPdata = Data(lipid_name, dataPath)
+                        experiments.append(Experiment(readmeExp, expOPdata, exp_subdir))
                 yaml_file_exp.close()
 
 #Pair each simulation with an experiment with the closest matching temperature and composition
 pairs = []
 
-for experiment in experiments: 
-    print(experiment.readme)
-    # check lipid composition matches the simulation
-    exp_lipids = experiment.getLipids() 
-      #  print(experiment.getLipids())
-
-    exp_total_lipid_concentration = experiment.readme['TOTAL_LIPID_CONCENTRATION']
-    exp_ions = experiment.getIons(ions_list)
-   # print('experiment ions: ' + str(exp_ions)) 
-    exp_counter_ions = experiment.readme['COUNTER_IONS']
-
-    for simulation in simulations:
-        sim_lipids = simulation.getLipids()
-        # continue if lipids are same
-        if set(sim_lipids) == set(exp_lipids):
-            sim_ions = simulation.getIons(ions_list)
-          #  print("simulation ions " + str(sim_ions)) 
-            sim_total_lipid_concentration = simulation.totalLipidConcentration() 
-            #print(lipids)
-            t_sim = simulation.readme['TEMPERATURE']
-            sim_molar_fractions = {}
-            #calculate molar fractions from simulation
-            for lipid in sim_lipids:
-                sim_molar_fractions[lipid] = simulation.molarFraction(lipid)
-  #          print(simulation.readme['SYSTEM'])
-  #          print(sim_molar_fractions)
-  #          print("total lipid concentration: " + str(sim_total_lipid_concentration))
-
-            #calculate concentrations of other molecules
-            sim_concentrations = {}
-            for molecule in ions_list:
-                sim_concentrations[molecule] = simulation.ionConcentration(molecule, exp_counter_ions)
-   #         print(sim_concentrations)
+for simulation in simulations:
+    sim_lipids = simulation.getLipids()
+    sim_total_lipid_concentration = simulation.totalLipidConcentration() 
+    sim_ions = simulation.getIons(ions_list)
+    t_sim = simulation.readme['TEMPERATURE']
     
+    #calculate molar fractions from simulation
+    sim_molar_fractions = {}
+    for lipid in sim_lipids:
+        sim_molar_fractions[lipid] = simulation.molarFraction(lipid)
+        
+        
+    for experiment in experiments: 
+       # print(experiment.readme)
+        # check lipid composition matches the simulation
+        exp_lipids = experiment.getLipids() 
+        #  print(experiment.getLipids())
+
+        exp_total_lipid_concentration = experiment.readme['TOTAL_LIPID_CONCENTRATION']
+        exp_ions = experiment.getIons(ions_list)
+        # print('experiment ions: ' + str(exp_ions)) 
+        exp_counter_ions = experiment.readme['COUNTER_IONS']
+        
+        # calculate simulation ion concentrations
+        sim_concentrations = {}
+        for molecule in ions_list:
+            sim_concentrations[molecule] = simulation.ionConcentration(molecule, exp_counter_ions)
 
         
+        # continue if lipid compositions are the same
+        if set(sim_lipids) == set(exp_lipids):
+            # compare molar fractions
             mf_ok = 0
             for key in sim_lipids:
                 if (experiment.readme['MOLAR_FRACTIONS'][key] >= sim_molar_fractions[key] - 0.05) and (experiment.readme['MOLAR_FRACTIONS'][key] <= sim_molar_fractions[key]+ 0.05):
@@ -240,7 +236,7 @@ for experiment in experiments:
 
             c_ok = 0
 
-
+            # compare ion concentrations 
             if set(sim_ions) == set(exp_ions):
               #  print(sim_ions)
               #  print(exp_ions)
@@ -258,28 +254,36 @@ for experiment in experiments:
                 if exp_total_lipid_concentration == sim_total_lipid_concentration:
                     switch = 1
                         
-            if switch == 1: 
-            #check temperature +/- 2 degrees
+            if switch == 1:
+            
+                #check temperature +/- 2 degrees
                 t_exp = experiment.readme['TEMPERATURE']
                 if (mf_ok == len(sim_lipids)) and (c_ok == len(sim_ions)) and (t_exp >= float(t_sim) - 2.0) and (t_exp <= float(t_sim) + 2.0):
                     #  print(simulation.indexingPath)
                     pairs.append([simulation, experiment])
-                    print(simulation.readme['SYSTEM'])
-                    print(simulation.indexingPath)
-                    print(experiment.dataPath)
+                   # print(simulation.readme['SYSTEM'])
+                   # print(simulation.indexingPath)
+                   # print(experiment.dataPath)
+                    
                     #Add path to experiment into simulation README.yaml
                     #many experiment entries can match to same simulation
                     exp_doi = experiment.readme['DOI'] 
-                    simulation.readme['EXPERIMENT'][exp_doi] = "/".join(experiment.dataPath.split("/")[4:7])
+                    lipid = experiment.data.molecule
+                    simulation.readme['EXPERIMENT'][lipid][exp_doi] = "/".join(experiment.dataPath.split("/")[4:7])
+                    print(simulation.readme['DOI'])
                     print(simulation.readme['EXPERIMENT'])
-                    outfileDICT = '../../Data/Simulations/'+ simulation.indexingPath + '/README.yaml'
+                    print('\n')
+                    
+    outfileDICT = '../../Data/Simulations/'+ simulation.indexingPath + '/README.yaml'
     
-                    with open(outfileDICT, 'w') as f:
-                        yaml.dump(simulation.readme,f, sort_keys=False)
+    with open(outfileDICT, 'w') as f:
+        yaml.dump(simulation.readme,f, sort_keys=False)
+    f.close()
+                        
 print("Found " + str(len(pairs)) + " pairs")  
-for pair in pairs:
-    print(pair[0].readme)
-    print(pair[1].readme)                        
+#for pair in pairs:
+#    print(pair[0].readme)
+#    print(pair[1].readme)                        
 
         
                 
