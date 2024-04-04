1fc0dee3e4f696596fc3db4b311041882e26903d
Alexey Nesterenko <comcon1@protonmail.com>
2024-04-04 16:32:42 +0200

    completely rebuild check_mappings for further debugging. It didn't work at all. Now it works.

--- Scripts/BuildDatabank/check_mappings.py
+++ Scripts/BuildDatabank/check_mappings.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# It checks mapping files, but going only through systems
+# which have TPR in folders preloaded. That is required for
+# debugging mappings code.
+#
+
 import os
 import sys
 import numpy as np
@@ -24,42 +32,41 @@ db_data = databank(path)
 systems = db_data.get_systems()
 
 for system in systems:
-    trj_name = system['path'] + system['TRJ'][0][0]
-    tpr_name = system['path'] + system['TPR'][0][0]
-    u = mda.Universe(tpr_name,trj_name)
+#    trj_name = system['path'] + system['TRJ'][0][0]
+    if 'TPR' not in system.keys():
+        sys.stderr.write("Skipping "+system["SYSTEM"]+" because there is no TPR\n")
+        continue
+    tpr_name = os.path.join(path, system['path'], system['TPR'][0][0])
+    if not os.path.exists(tpr_name):
+        sys.stderr.write("Skipping "+system["SYSTEM"]+" because TPR is not downloaded\n")
+        sys.stderr.write("==> "+tpr_name+"\n")
+        continue
+    print("Checking system "+system['path']+" ...")
+    # opening Universe using TPR only
+    tpars = mda.topology.TPRParser.TPRParser(tpr_name)
+    tpl = tpars.parse()
+    u = mda.Universe(tpl)
     for molecule in system['COMPOSITION']:
-        #print(system['COMPOSITION'][molecule]['MAPPING'])
+        errnum = 0
         m_file = system['COMPOSITION'][molecule]['MAPPING']
-        with open('./mapping_files/'+m_file,"r") as f:
-            for line in f:
-                #if len(line.split()) > 2 and "Individual atoms" not in line:
-                #    selection = 'resname ' + line.split()[2] + ' and name ' +  line.split()[1] 
-                #el
-                selection = ''
-                try:
-                    if system['UNITEDATOM_DICT'] and 'H' in line.split()[0]:
-                        continue
-                    elif "Individual atoms" in line:
-                        continue
-                    elif len(line.split()) > 2 and "Individual atoms" not in line:
-                        selection = 'resname ' + line.split()[2] + ' and name ' +  line.split()[1] 
-                    else:
-                        selection = 'resname ' + system['COMPOSITION'][molecule]['NAME'] + ' and name ' +  line.split()[1]
-                except:
-                    if "Individual atoms" in line:
-                        continue
-                    elif len(line.split()) > 2:
-                        selection = 'resname ' + line.split()[2] + ' and name ' +  line.split()[1] 
-                    else:
-                        selection = 'resname ' + system['COMPOSITION'][molecule]['NAME'] + ' and name ' +  line.split()[1]
-
+        # opening mapping-yaml
+        mapping_dict = {}
+        with open('./mapping_files/'+m_file, "r") as yaml_file:
+            mapping_dict = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        # go over all records
+        for mk in mapping_dict.keys():
+                selection = 'resname ' + system['COMPOSITION'][molecule]['NAME'] + ' and name ' +  mapping_dict[mk]['ATOMNAME']
                 NatomsFromMapping = len(u.select_atoms(selection))
                 NatomsFromREADME = np.sum(system['COMPOSITION'][molecule]['COUNT'])
-                #print(len(u.select_atoms(selection)), np.sum(system['COMPOSITION'][molecule]['COUNT']))
                 if NatomsFromMapping !=  NatomsFromREADME and molecule not in lipids_dict:
-                    print(system['path'])
-                    #os.system('rm ' + system['path'] + '/*Density*')
-                    #os.system('rm ' + system['path'] + '/FormFactor*')
-                    print(system['COMPOSITION'][molecule]['NAME'])
-                    print(selection)
-                    print(NatomsFromMapping, NatomsFromREADME)
+                    print("""
+Found problematic system: %s
+Molecule named %s
+MDA selection: "%s"
+Atoms from mapping/in readme: %d / %d
+""" %
+     (system['SYSTEM'], system['COMPOSITION'][molecule]['NAME'], selection, 
+     NatomsFromMapping, NatomsFromREADME) )
+                    errnum += 1
+                # end for
+        print("Everything is OK for molecule %s!" % molecule)
