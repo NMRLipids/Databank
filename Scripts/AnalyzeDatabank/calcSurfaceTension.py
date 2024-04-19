b4cd6243c215ffa018f1b8a7bd38822db5bef62e
fsuarezleston <f.suarez.leston@gmail.com>
2024-04-19 17:26:50 +0200

    Change the way to compute surface tension

--- Scripts/AnalyzeDatabank/calcSurfaceTension.py
+++ Scripts/AnalyzeDatabank/calcSurfaceTension.py
@@ -36,21 +36,44 @@ for system in systems:
             systemPath = os.path.dirname(os.path.realpath(__file__)) + '/../../Data/Simulations/' + system['path'] 
             doi = system.get('DOI')
             edr = system.get('EDR')
+            tpr = system.get('TPR')
             edr_name = systemPath + system.get('EDR')[0][0]
             edr_url = download_link(doi, edr[0][0])
+            tpr_name = systemPath + system.get('TPR')[0][0]
+            tpr_url = download_link(doi, tpr[0][0])
             
             # Download the EDR file if not available
             if (not os.path.isfile(edr_name)):
                 print('Downloading the EDR file to ', system['path'])
                 response = urllib.request.urlretrieve(edr_url, edr_name)
+                
+            # Download the TPR file if not available
+            if (not os.path.isfile(tpr_name)):
+                print('Downloading the TPR file to ', system['path'])
+                response = urllib.request.urlretrieve(tpr_url, tpr_name)
+                
+            # Generate a PDB file from the TPR
+            os.system( f'gmx editconf -f {tpr_name} -o {systemPath}conf.pdb >/dev/null 2>&1' )
+            
+            # Get the size of the box in the Z dimension
+            u = MDAnalysis.Universe( tpr_name, systemPath + 'conf.pdb' )
+            Lz = u.dimensions[2]
             
             edata = MDAnalysis.auxiliary.EDR.EDRReader( edr_name )
             
-            surft = { time: value for time, value in zip( edata.data_dict["Time"][ edata.data_dict["Time"] >= system['TIMELEFTOUT']*1000 ],
-                                                          edata.data_dict['#Surf*SurfTen'][ edata.data_dict["Time"] >= system['TIMELEFTOUT']*1000 ]/2 ) } 
+            frames = edata.data_dict["Time"] >= system['TIMELEFTOUT']*1000
+            
+            Pxx = edata.data_dict['Pres-XX'][ frames ]
+            Pyy = edata.data_dict['Pres-YY'][ frames ]
+            Pzz = edata.data_dict['Pres-ZZ'][ frames ]
+            
+            surft = { time: value for time, value in zip( edata.data_dict["Time"][ frames ],
+                                                          Lz*(Pzz-0.5*(Pxx+Pyy))/2 * 0.01 ) } 
         
             with open(outfilename, 'w') as f:
                 json.dump(surft,f)
+                
+            os.remove( systemPath + 'conf.pdb' )
         
         except:
             print ('It was not possible to compute the surface tension of the system.')
