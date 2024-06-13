b0aa78e3a7d28d55d39fae006d9135e2cef3be67
fsuarezleston <f.suarez.leston@gmail.com>
2024-06-13 10:35:04 +0200

    Add experimental data for monolayers

---
+++ Data/experiments/Isotherms/data_to_json.py
@@ -0,0 +1,30 @@
+import sys
+import json
+
+data_file = sys.argv[1]
+print(data_file)
+
+if data_file.endswith(".dat"):
+    ext = ".dat"
+    sep = None
+elif data_file.endswith(".csv"):
+    ext = ".csv"
+    sep = ","
+    
+outfile = data_file.replace(ext,"_Isotherms.json")
+
+data = []
+
+with open(data_file) as PAfile:
+    lines=PAfile.readlines()
+    for line in lines:
+        if "#" in line or line == "\n":
+            continue
+        values = [ float(i) for i in line.split(sep) ] 
+
+        data.append(values)
+        
+        with open(outfile, 'w') as f:
+            json.dump(data,f)
+    
+print(outfile)
