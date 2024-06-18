0f55b4f7065375b77a17a4f951276ace5010c928
Alexey Nesterenko <comcon1@protonmail.com>
2024-06-18 12:52:33 +0200

    Fix JSON formatter and other behavior of data2json script. Convert my experiments with new formatter. Test that QualityEvaluation understands newly formatted jsons. Partially solves #178

--- Data/experiments/OrderParameters/data_to_json.py
+++ Data/experiments/OrderParameters/data_to_json.py
@@ -1,57 +1,42 @@
-#Organize experimental data into a databank
-#Amount of each molecule in the membrane is given as a ratio. For a membrane of single molecule it's 1.
+#!/usr/bin/env python3
+""""
+Converting OP dat file into properly organized JSON format.
+- Amount of each molecule in the membrane is given as a ratio.
+- For a membrane of single molecule it's 1.
+"""
 
-import os
-import numpy as np
-
-import yaml
-import json
+import os, yaml, json, re, math
 
 import sys
-
-#with open(sys.argv[1], 'r') as f:
-#    contents = f.read()
-#print(contents)
+sys.path.append('../../../Scripts/BuildDatabank')
+from jsonEncoders import CompactJSONEncoder
 
 data_file = sys.argv[1]
-print(data_file)
+print("Converting from: ", data_file)
 outfile = data_file.replace(".dat",".json")
-print(outfile)
+print("Converting to: ", outfile)
 
-#data_file ="/media/akiirikk/DATADRIVE1/tietokanta/NMRLipids_Databank/Databank/Data/experiments/OrderParameters/10.1039/c2cp42738a/4/POPC_Order_Parameters.dat"
-
-#save_dir = "/media/akiirikk/DATADRIVE1/tietokanta/NMRLipids_Databank/Databank/Data/experiments/OrderParameters/10.1039/c2cp42738a/4/"
-
-#experiment information
-
-#save_info = {}
-#save_info['DOI'] = "10.5281/zenodo.47488647"
-#save_info['TEMPERATURE'] = 310
-#save_info['MOLECULE_FRACTIONS'] = {'POPE': 1}
-#print(save_info)
-
-#outfileDICT = str(save_dir) + '/README.yaml'
-    
-#with open(outfileDICT, 'w') as f:
-#    yaml.dump(save_info,f, sort_keys=False)
-
-
-#write data in json
-
-#outfile = save_dir + "POPC_Order_Parameters.json"
 data = {}
 
+DEFAULT_OP_ERROR = 0.02
+
 with open(data_file) as OPfile:
-    lines=OPfile.readlines()
+    lines = OPfile.readlines()
     for line in lines:
-        if "#" in line or line == "\n":
+        line = re.sub(r'#.*$', '', line).strip()
+        if line == "":
             continue
-        print(line.split())
-        OPname = line.split()[0] + " " + line.split()[1]
-        OPvalues = [float(line.split()[2])] #, float(line.split()[3])]
-        data[str(OPname)]=[]
-        data[str(OPname)].append(OPvalues)
+        lSplit = line.split()
+        print(lSplit)
+        assert(len(lSplit) in [3,4])
+        if math.isnan(float(lSplit[2])):
+            continue
+        OPname = lSplit[0] + " " + lSplit[1]
+        err = DEFAULT_OP_ERROR if len(lSplit) == 3 else float(lSplit[3])
+        OPvalues = [ float(lSplit[2]),  err ]
+        data[str(OPname)] = [ OPvalues ]
+        #TODO: remove this double list thing!
         
-        with open(outfile, 'w') as f:
-            json.dump(data,f)
+with open(outfile, 'w') as f:
+    json.dump(data,f, cls=CompactJSONEncoder)
 
