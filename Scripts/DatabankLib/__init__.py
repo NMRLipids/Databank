1f3f9c0077f338951bdf99fbe4070feba3861f3e
Alexey Nesterenko <comcon1@protonmail.com>
2024-07-03 10:41:59 +0200

    implement absolute paths at package __init__ level

--- Scripts/DatabankLib/__init__.py
+++ Scripts/DatabankLib/__init__.py
@@ -2,3 +2,14 @@
 For now we just need to declare this folder to be package.
 TODO: organize proper package structure
 """
+
+#TODO: think which modules should be imported at package-level
+# from . import some_module
+
+import os
+
+NMLDB_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+NMLDB_DATA_PATH = os.path.join(NMLDB_ROOT_PATH, 'Data')
+NMLDB_SIMU_PATH = os.path.join(NMLDB_ROOT_PATH, 'Data', 'Simulations')
+NMLDB_EXP_PATH  = os.path.join(NMLDB_ROOT_PATH, 'Data', 'experiments')
