01e378421072ca1ff80a3a80ea7c331035d36397
comcon1 <comcon1@protonmail.com>
2024-08-05 22:59:17 +0200

    add calculateOP and its test-function

--- Scripts/tests/test_analyze.py
+++ Scripts/tests/test_analyze.py
@@ -5,7 +5,7 @@ import DatabankLib
 
 @pytest.fixture(autouse=True, scope="session")
 def header_session_scope():
-    _rp = os.path.join(os.path.dirname(__file__), "Data", "Simulations")
+    _rp = os.path.join(os.path.dirname(__file__), "Data", "Simulations.1")
     with mock.patch.object(DatabankLib, "NMLDB_SIMU_PATH", _rp):
         print("DBG: Mocking simulation path: ", DatabankLib.NMLDB_SIMU_PATH)
         yield
@@ -18,8 +18,16 @@ def systems():
     print(f"Loaded: {len(s)} systems")
     yield s
     ## TEARDOWN SYSTEMS
+    print('DBG: Wiping temporary calculation data.')
     for _s in s:
-        for f in glob.glob(os.path.join(DatabankLib.NMLDB_SIMU_PATH, _s['path'], '*.json')):
+        gbGen = lambda x: glob.glob(os.path.join(DatabankLib.NMLDB_SIMU_PATH, _s['path'], x))
+        for f in gbGen('*.json'):
+            os.remove(f)
+        for f in gbGen('*.xtc'):
+            os.remove(f)
+        for f in gbGen('*.dat'):
+            os.remove(f)
+        for f in gbGen('conf.gro'):
             os.remove(f)
 
 
@@ -46,9 +54,23 @@ def test_analyze_apl(systems, systemLoadTraj):
     aplCreated = []
     aplNZ = []
     for s in systems:
-        computeAPL(s)
+        rCode = computeAPL(s)
         cFile = os.path.join(DatabankLib.NMLDB_SIMU_PATH, 
                                         s['path'], 'apl.json')
         aplCreated.append( os.path.isfile(cFile) )
         aplNZ.append( os.path.getsize(cFile) > 1e3 )
     assert all(aplCreated) and all(aplNZ)
+
+
+def test_analyze_op(systems, systemLoadTraj):
+    from DatabankLib.analyze import computeOP
+    opCreated = []
+    opNZ = []
+    for s in systems:
+        rCode = computeOP(s)
+        # check only 1st lipid
+        cFile = os.path.join(DatabankLib.NMLDB_SIMU_PATH, 
+            s['path'], list(s['COMPOSITION'].keys())[0] + 'OrderParameters.json')
+        opCreated.append( os.path.isfile(cFile) )
+        opNZ.append( os.path.getsize(cFile) > 1e3 )
+    assert all(opCreated) and all(opNZ) 
