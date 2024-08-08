3bf00e64f37929f62d682af5e30c3d1b547a9006
comcon1 <comcon1@protonmail.com>
2024-08-08 23:55:41 +0200

    next block of implementation of tests

--- Scripts/DatabankLib/__init__.py
+++ Scripts/DatabankLib/__init__.py
@@ -13,3 +13,15 @@ NMLDB_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspat
 NMLDB_DATA_PATH = os.path.join(NMLDB_ROOT_PATH, 'Data')
 NMLDB_SIMU_PATH = os.path.join(NMLDB_ROOT_PATH, 'Data', 'Simulations')
 NMLDB_EXP_PATH  = os.path.join(NMLDB_ROOT_PATH, 'Data', 'experiments')
+
+## Universal success codes
+
+RCODE_SKIPPED: int = 0
+""" Success code 0: calculation skipped """
+
+RCODE_COMPUTED: int = 1
+""" Success code 1: calculation sucessful """
+
+RCODE_ERROR: int = 2
+""" Success code 2: calculation failed """
+
