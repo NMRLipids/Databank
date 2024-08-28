0f1e7324237f7c8cf53871ea49b08b1db4766757
comcon1 <comcon1@protonmail.com>
2024-08-28 22:09:22 +0200

    complete moving code from CLI to analyze.py module

--- Scripts/AnalyzeDatabank/NMRPCA_timerelax.py
+++ Scripts/AnalyzeDatabank/NMRPCA_timerelax.py
@@ -12,81 +12,30 @@ whole, or GROMACS is installed and available via gmx command.
 For details on NMRPCA, see `DatabankLib/analyze_nmrpca.py`.
 """
 
-import gc
 import os, sys
-import warnings
+
+import logging
+logger = logging.getLogger(__name__)
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from DatabankLib import NMLDB_SIMU_PATH
-from DatabankLib.analyze_nmrpca import *
-from DatabankLib.databankLibrary import initialize_databank, lipids_dict
+import DatabankLib
+from DatabankLib.core import initialize_databank
+from DatabankLib.analyze import computeNMRPCA
 
-if __name__ == "__main__":
-    # TODO: do we need these warnings?
-    warnings.filterwarnings("ignore", category=UserWarning)
 
-    # searches through every subfolder of a path
-    # and finds every trajectory in databank
-    systems = initialize_databank()
+systems = initialize_databank()
+resDict = {DatabankLib.RCODE_COMPUTED: 0, 
+           DatabankLib.RCODE_SKIPPED: 0,
+           DatabankLib.RCODE_ERROR: 0}
 
-    i = 0
-    listall = []
+for system in systems:
+    logger.info("System title: " + system['SYSTEM'])
+    logger.info("System path: " + system['path'])
+    res = computeNMRPCA(system)
+    resDict[res] += 1
 
-    eq_time_fname = "eq_times.json"
-
-    for readme in systems:
-        print('== Doi: ' + readme['DOI'])
-        print('== System name: ' + readme['SYSTEM'])
-        # getting data from databank and preprocessing them
-        # Start Parser
-        # TODO: 2test|    parser = Parser(NMLDB_SIMU_PATH, readme, eq_time_fname, testTraj)
-        parser = Parser(NMLDB_SIMU_PATH, readme, eq_time_fname)
-        print(parser.indexingPath)
-        print(parser.validatePath())
-        if parser.validatePath() < 0:
-            continue
-        # Download files
-
-        eq_time_path = os.path.join(NMLDB_SIMU_PATH, readme['path'], "eq_times.json")
-        print(eq_time_path)
-        if os.path.isfile(eq_time_path):
-            continue
-
-        if ('WARNINGS' in readme and 
-            readme['WARNINGS'] is not None and 
-            'AMBIGUOUS_ATOMNAMES' in readme['WARNINGS']):
-            continue
-
-        if ('WARNINGS' in readme and 
-            readme['WARNINGS'] is not None and
-            'SKIP_EQTIMES' in readme['WARNINGS']):
-            continue
-
-        parser.downloadTraj()
-        # Prepare trajectory
-        parser.prepareTraj()
-        # Concatenate trajectory
-        parser.concatenateTraj()
-        equilibration_times = {}
-        # Iterate over trajectories for different lipids
-        for traj in parser.concatenated_trajs:
-            print(f"Main: parsing lipid {traj[4]}")
-            # Creat PCA for trajectory
-            pca_runner = PCA(traj[0], traj[1], traj[2], traj[3], parser.trjLen)
-            # Run PCA
-            data = pca_runner.PCA()
-            print("Main: PCA: done")
-            # Project trajectory on PC1
-            pca_runner.get_proj(data)
-            print("Main: Projections: done")
-            # Calculate autocorrelations
-            pca_runner.get_autocorrelations()
-            print("Main: Autocorrelations: done")
-            # Estimate equilibration time
-            te2 = TimeEstimator(pca_runner.autocorrelation).calculate_time()
-            equilibration_times[traj[4]] = te2 / parser.trjLen
-            print("Main: EQ time: done")
-
-            print(te2 / parser.trjLen)
-        gc.collect()
-        parser.dumpData(equilibration_times)
+print(f"""
+COMPUTED: {resDict[DatabankLib.RCODE_COMPUTED]}
+ SKIPPED: {resDict[DatabankLib.RCODE_SKIPPED]}
+   ERROR: {resDict[DatabankLib.RCODE_ERROR]}
+""")
--- Scripts/AnalyzeDatabank/calc_thickness.py
+++ Scripts/AnalyzeDatabank/calc_thickness.py
@@ -2,54 +2,29 @@
 # coding: utf-8
 
 import sys, os
-import numpy as np
-import json
-from tqdm import tqdm
-import traceback
+
+import logging
+logger = logging.getLogger(__name__)
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from DatabankLib.databankLibrary import initialize_databank
-from DatabankLib import NMLDB_SIMU_PATH
+import DatabankLib
+from DatabankLib.core import initialize_databank
+from DatabankLib.analyze import computeTH
+
 
 systems = initialize_databank()
+resDict = {DatabankLib.RCODE_COMPUTED: 0, 
+           DatabankLib.RCODE_SKIPPED: 0,
+           DatabankLib.RCODE_ERROR: 0}
 
-cErr = 0
-cUpp = 0
-
-for system in tqdm(systems, desc='Scan over Databank systems'):
-    thickFN = os.path.join(NMLDB_SIMU_PATH, system['path'], 'thickness.json')
-    if (os.path.isfile(thickFN)):
-        # file exist. Skipping
-        continue
-
-    WaterDensity_name = os.path.join(NMLDB_SIMU_PATH, system['path'], 'WaterDensity.json')
-    LipidDensity_name = os.path.join(NMLDB_SIMU_PATH, system['path'], 'LipidDensity.json')
-    print(LipidDensity_name)
-    try:
-        with open(WaterDensity_name) as f:
-            WaterDensity = json.load(f)
-        with open(LipidDensity_name) as f:
-            LipidDensity = json.load(f)
-        wd = np.array(WaterDensity)
-        ld = np.array(LipidDensity)
-        idx = np.argwhere(np.diff(np.sign(wd[:,1] - ld[:,1]))).flatten()
-        if len(idx) < 2:
-            print("Dehydrated sample! Standard thickness rule doesn't work. Will extract boxZ.")
-            thickness = wd[-1, 0] - wd[0, 0]
-        else:
-            thickness = wd[idx[1], 0] - wd[idx[0], 0]
-        with open(thickFN, 'w') as f:
-            json.dump(thickness, f)
-            cUpp += 1
-    except Exception as e:
-        print('Calculation failed for ' +  system['path'])
-        print(str(e))
-        print(traceback.format_exc())
-        cErr += 1
+for system in systems:
+    logger.info("System title: " + system['SYSTEM'])
+    logger.info("System path: " + system['path'])
+    res = computeTH(system)
+    resDict[res] += 1
 
 print(f"""
-===========================================
-Summary:
- Thickness updated for: {cUpp} systems
- Thickness calculation failed: {cErr} systems
+COMPUTED: {resDict[DatabankLib.RCODE_COMPUTED]}
+ SKIPPED: {resDict[DatabankLib.RCODE_SKIPPED]}
+   ERROR: {resDict[DatabankLib.RCODE_ERROR]}
 """)
