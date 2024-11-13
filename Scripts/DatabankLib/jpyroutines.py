ffeb30c695220add2d061fcda80cd1b9767d71d5
Alexey Nesterenko <comcon1@protonmail.com>
2024-11-13 15:46:10 +0100

    fix naming in jpyroutines.py; add TODO about naming style-convention

--- Scripts/DatabankLib/jpyroutines.py
+++ Scripts/DatabankLib/jpyroutines.py
@@ -8,45 +8,45 @@ from IPython.display import display
 from IPython.display import Markdown
 
 
-def ShowTable(SortedQualities, quality):
+def showTable(sorted_qualities, quality):
     """
     Shows a table of simulation qualities against experimental data.
 
-    :param SortedQualities: list of dictionaries to be shown, available
+    :param sorted_qualities: list of dictionaries to be shown, available
                             in folder ``Data/Ranking/``
     :param quality: should be either ``TotalQuality`` or universal lipid name.
                     First one shows the system total quality.
                     Latter shows the individual lipid quality.
     """
     rounding = ['headgroup', 'sn-1', 'sn-2', 'total', 'tails', 'FFQuality']
-    QualityTable = []
+    quality_table = []
     pd.set_option('display.max_rows', None)
-    for i in SortedQualities:
-        StoredToTable = []
+    for i in sorted_qualities:
+        stored_to_table = []
         for k, v in i[quality].items():
             if k in rounding:
                 if v and v != float("inf") and not np.isnan(v):
                     i[quality][k] = round(float(v), 2)
 
-        StoredToTable = i[quality]
+        stored_to_table = i[quality]
         try:
-            StoredToTable['Forcefield'] = i['system']['FF']
+            stored_to_table['Forcefield'] = i['system']['FF']
         except (KeyError, TypeError):
             display(Markdown('**FAILURE:** no FF defined for the system'))
             display(i['system']['path'])
             continue
 
         molecules = ''
-        MolNumbers = ''
+        mol_numbers = ''
         for lipid in i['system']['COMPOSITION']:
             molecules = molecules + lipid + ':'
-            MolNumbers = (
-                MolNumbers +
+            mol_numbers = (
+                mol_numbers +
                 str(np.sum(i['system']['COMPOSITION'][lipid]['COUNT'])) +
                 ':')
-        StoredToTable['Molecules'] = molecules[:-1]
-        StoredToTable['Number of molecules'] = ' (' + MolNumbers[:-1] + ')'
-        StoredToTable['Temperature'] = i['system']['TEMPERATURE']
-        StoredToTable['ID'] = i['system']['ID']
-        QualityTable.append(StoredToTable)
-    display(pd.json_normalize(QualityTable))
+        stored_to_table['Molecules'] = molecules[:-1]
+        stored_to_table['Number of molecules'] = ' (' + mol_numbers[:-1] + ')'
+        stored_to_table['Temperature'] = i['system']['TEMPERATURE']
+        stored_to_table['ID'] = i['system']['ID']
+        quality_table.append(stored_to_table)
+    display(pd.json_normalize(quality_table))
