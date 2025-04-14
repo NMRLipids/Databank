05d116b699dd0130ba76408851ccdfb2d9ded1f8
comcon1 <comcon1@protonmail.com>
2025-04-14 21:30:22 +0200

    compare arrays via np.testing.assert_allclose which resolves @batukav comment on PR #218

--- Scripts/tests/test_analyze.py
+++ Scripts/tests/test_analyze.py
@@ -121,23 +121,22 @@ def compareJSONsBtwSD(jsfn: str):
     if type(j1) is list:
         assert len(j1) == len(j2), \
             f"Problem in {jsfn} comparison: lists has different lengths!"
-        for k1 in range(len(j1)):
-            _a = np.abs(np.array(j1[k1]) - np.array(j2[k1])).ravel()
-            _b = np.abs((np.array(j1[k1]) + np.array(j2[k1])).ravel())/2
-            _b[_b == 0] = MAXRELERR_COMPARE_THRESHOLD
-            assert np.max(_a/_b) < MAXRELERR_COMPARE_THRESHOLD, (
-               f"Problem in {jsfn} comparison: {k1} line.\n" +
-               f"Computed: {str(j1[k1])} \n" +
-               f"Pre-computed: {str(j2[k1])}")
-    elif type(j1) is dict:
+        # to process further list as dict
+        j1 = dict(enumerate(j1))
+        j2 = dict(enumerate(j2))
+
+    if type(j1) is dict:
+        assert(set(j1.keys())==set(j2.keys()))
         for k1 in j1:
-            _a = np.abs(np.array(j1[k1]) - np.array(j2[k1])).ravel()
-            _b = np.abs((np.array(j1[k1]) + np.array(j2[k1])).ravel())/2
-            _b[_b == 0] = MAXRELERR_COMPARE_THRESHOLD
-            assert np.max(_a/_b) < MAXRELERR_COMPARE_THRESHOLD, (
-               f"Problem in {jsfn} comparison: {k1} field.\n" +
-               f"Computed: {str(j1[k1])} \n" +
-               f"Pre-computed: {str(j2[k1])}")
+            np.testing.assert_allclose(
+                np.array(j1[k1]),
+                np.array(j2[k1]),
+                rtol=MAXRELERR_COMPARE_THRESHOLD,
+                err_msg=(
+                    f"Problem in {jsfn} comparison: {k1} line.\n" +
+                    f"Computed: {str(j1[k1])} \n" +
+                    f"Pre-computed: {str(j2[k1])}")
+            )
 
     print(f"Data {jsfn} was compared against precomputed!")
 
