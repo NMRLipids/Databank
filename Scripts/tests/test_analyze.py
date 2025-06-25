666f392386f63e0f9ae55c34a1abeaaefdf95b0e
comcon1 <comcon1@protonmail.com>
2025-06-25 18:44:14 +0200

    NOWIPE leaves JSONs. MAICoS saver now have correct path prefix.

--- Scripts/tests/test_analyze.py
+++ Scripts/tests/test_analyze.py
@@ -36,9 +36,15 @@ def systems():
     for _s in s:
         def gbGen(x): return glob.glob(
                     os.path.join(DatabankLib.NMLDB_SIMU_PATH, _s['path'], x))
-        clearList = ['*.json', '*.dat', 'conf.gro', 'frame0.gro', '*.dat',
+        clear_list = []
+        if os.environ["NMLDB_TEST_NOWIPE"] == "1":
+            # leave JSONs
+            pass
+        else:
+            clear_list = ['*.json']
+        clear_list += ['*.dat', 'conf.gro', 'frame0.gro', '*.dat',
                      '*.buildH', '.*', '#*', '*.def', 'whole.xtc', 'centered.xtc']
-        for pat in clearList:
+        for pat in clear_list:
             for f in gbGen(pat):
                 os.remove(f)
 
