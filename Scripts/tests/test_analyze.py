5738002fd072c84dd137f70f02107693b551a141
batukav <batuhankav@gmail.com>
2025-07-08 14:01:18 +0200

    change logging level from INFO to DEBUG

--- Scripts/tests/test_analyze.py
+++ Scripts/tests/test_analyze.py
@@ -19,7 +19,7 @@ logger.setLevel(logging.DEBUG)
 
 # Create console handler with a higher log level
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+ch.setLevel(logging.DEBUG)
 
 # Create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
