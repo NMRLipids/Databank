61bcc54c4944672d25d8e34ce95549271216a2ad
MagnusSletten <magnus.elias.sletten@gmail.com>
2025-09-06 10:07:55 +0200

    Satisfy ruff requirements on workflowscripts

--- Scripts/WorkflowScripts/ProcessInfoFile.py
+++ Scripts/WorkflowScripts/ProcessInfoFile.py
@@ -1,8 +1,17 @@
+"""
+Executes standard pipeline of processing for an info file.
+
+.. note::
+   This file is only meant to be used by automated workflows.
+   Users of the Databank repository can safely ignore it.
+"""
+
 import argparse
 import os
+import sys
 
 from DatabankLib import NMLDB_ROOT_PATH
-from WorkflowScripts.Workflow_utils import *
+from WorkflowScripts.Workflow_utils import delete_info_file, get_databank_paths, logger, run_python_script
 
 """
 Executes standard pipeline of processing for an info file.
@@ -13,7 +22,8 @@ Executes standard pipeline of processing for an info file.
 """
 
 
-def main(info_file_path):
+def main(info_file_path : str) ->None:
+    """Run methods AddData and simulation analysis scripts."""
     path_dict = get_databank_paths(NMLDB_ROOT_PATH)
     parent_folder = os.path.dirname(NMLDB_ROOT_PATH)
 
@@ -24,16 +34,16 @@ def main(info_file_path):
         os.makedirs(work_directory_dry, exist_ok=True)
         os.makedirs(work_directory_real, exist_ok=True)
     except OSError:
-        logger.error("Failed to create work directories")
+        logger.exception("Failed to create work directories.")
         sys.exit(1)
 
     run_python_script(
-        path_dict["AddData_path"],
+        path_dict["adddata_path"],
         args=["-f", info_file_path, "-w", work_directory_dry, "--dry-run"],
         error_message="AddData dry run failed",
     )
     run_python_script(
-        path_dict["AddData_path"],
+        path_dict["adddata_path"],
         args=["-f", info_file_path, "-w", work_directory_real],
         error_message="AddData failed",
     )
@@ -46,9 +56,10 @@ def main(info_file_path):
 
 
 # Gets arguments from parser
-def get_args():
+def get_args() -> argparse.Namespace:
+    """Return arguments from parser."""
     parser = argparse.ArgumentParser(description="Run NMRLipids data pipeline on a YAML info file.")
-    parser.add_argument("--info_file_path", required=True, help="Path to the info.yml file")
+    parser.add_argument("--info_file_path", required=True, help="Path to the info.yml file.")
     return parser.parse_args()
 
 
@@ -56,6 +67,6 @@ if __name__ == "__main__":
     args = get_args()
     info_file_path = args.info_file_path
     if not info_file_path:
-        print("No path provided, exiting.")
+        logger.info("No path provided, exiting.")
         sys.exit(0)
     main(info_file_path)
--- Scripts/WorkflowScripts/RunAnalysis.py
+++ Scripts/WorkflowScripts/RunAnalysis.py
@@ -1,20 +1,21 @@
-from DatabankLib import NMLDB_ROOT_PATH
-from WorkflowScripts.Workflow_utils import *
-
 """
-Executes pipeline of analysis methods that work globally on simulations.
+Execute pipeline of analysis methods that work globally on simulations.
 
 .. note::
    This file is only meant to be used by automated workflows.
    Users of the Databank repository can safely ignore it.
 """
 
+from DatabankLib import NMLDB_ROOT_PATH
+from WorkflowScripts.Workflow_utils import get_databank_paths,run_python_script
 
-def main():
+
+def main() -> None:
+    """Run analysis scripts on simulations."""
     path_dict = get_databank_paths(NMLDB_ROOT_PATH)
-    run_python_script(path_dict["searchDATABANK_path"], error_message="SearchDatabank failed")
-    run_python_script(path_dict["QualityEvaluation_path"], error_message="QualityEvaluation failed")
-    run_python_script(path_dict["makeRanking_path"], error_message="makeRanking failed")
+    run_python_script(path_dict["searchdatabank_path"], error_message="SearchDatabank failed")
+    run_python_script(path_dict["qualityevaluation_path"], error_message="QualityEvaluation failed")
+    run_python_script(path_dict["makeranking_path"], error_message="MakeRanking failed")
 
 
 if __name__ == "__main__":
