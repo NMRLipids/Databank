071c781c7f6377659b2b387f6c0d2134d20ab8c2
MagnusSletten <magnus.elias.sletten@gmail.com>
2025-09-12 23:24:56 +0200

    Refactor ProcessInfoFile

--- Scripts/WorkflowScripts/ProcessInfoFile.py
+++ Scripts/WorkflowScripts/ProcessInfoFile.py
@@ -1,10 +1,3 @@
-import argparse
-import os
-import sys
-
-from DatabankLib import NMLDB_ROOT_PATH
-from WorkflowScripts.Workflow_utils import delete_info_file, get_databank_paths, logger, run_python_script
-
 """
 Executes standard pipeline of processing for an info file.
 
@@ -13,8 +6,42 @@ Executes standard pipeline of processing for an info file.
    Users of the Databank repository can safely ignore it.
 """
 
-def main(info_file_path:str, dry_run: bool): 
+import argparse
+import os
+import sys
+
+from DatabankLib import NMLDB_ROOT_PATH
+from WorkflowScripts.Workflow_utils import delete_info_file, get_databank_paths, logger, run_python_script
+
+
+def run_analysis(info_file_path:str) -> None:
+    """Run full analysis of info-file"""
+    work_directory_real,_ = setup_folders()
     path_dict = get_databank_paths(NMLDB_ROOT_PATH)
+    run_python_script(
+        path_dict["adddata_path"],
+        args=["-f", info_file_path, "-w", work_directory_real],
+        error_message="AddData failed"
+    )
+    run_python_script(
+        path_dict["compute_databank_path"],
+        args = ["--ff", "--range", "*-0"],
+        error_message="Compute_databank failed"
+    )
+    delete_info_file(info_file_path)
+
+
+def run_dry_run(info_file_path:str) -> None:
+    """Run pre-analysis of info file to rule out errors"""
+    _,work_directory_dry = setup_folders()
+    path_dict = get_databank_paths(NMLDB_ROOT_PATH)
+    run_python_script(
+            path_dict["adddata_path"],
+            args=["-f", info_file_path, "-w", work_directory_dry, "--dry-run"],
+            error_message="AddData dry run failed")
+    
+def setup_folders() -> tuple[str,str]:
+    """Set up folders for processing info file"""
     parent_folder = os.path.dirname(NMLDB_ROOT_PATH)
     base_tmp = os.path.join(parent_folder, "databank_workdir")
     work_directory_dry  = os.path.join(base_tmp, "dry")
@@ -23,28 +50,13 @@ def main(info_file_path:str, dry_run: bool):
         os.makedirs(work_directory_dry, exist_ok=True)
         os.makedirs(work_directory_real, exist_ok=True)
     except OSError as e:
-        logger.error(f"Failed to create work directories")
+        logger.error("Failed to create work directories")
         sys.exit(1)
-    if dry_run:
-        run_python_script(
-            path_dict["adddata_path"],
-            args=["-f", info_file_path, "-w", work_directory_dry, "--dry-run"],
-            error_message="AddData dry run failed")
-    else:    
-        run_python_script(
-            path_dict["adddata_path"],
-            args=["-f", info_file_path, "-w", work_directory_real],
-            error_message="AddData failed"
-        )
-        run_python_script(
-            path_dict["compute_databank_path"],
-            args = ["--ff", "--range", "*-0"],
-            error_message="Compute_databank failed"
-        )
-        delete_info_file(info_file_path)
+    return work_directory_real,work_directory_dry
 
-#Gets arguments from parser
-def get_args():
+
+def get_args() -> argparse.Namespace:
+    """Retrieve arguments from parser"""
     parser = argparse.ArgumentParser(description="Run NMRLipids data pipeline on a YAML info file.")
     parser.add_argument("--info_file_path", required=True, help="Path to the info.yml file")
     parser.add_argument("--dry-run", action="store_true", help="Only run preanalysis (no compute/commit)")
@@ -55,6 +67,10 @@ if __name__ == "__main__":
     args = get_args()
     info_file_path = args.info_file_path
     if not info_file_path:
-        print("No path provided, exiting.")
+        logger.info("No path provided, exiting.")
         sys.exit(0)
-    main(info_file_path,dry_run=args.dry_run)
+    if args.dry_run:
+        run_dry_run(info_file_path)
+    else:
+        run_analysis(info_file_path)
+
